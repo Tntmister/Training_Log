fd0e392800ef7cd5bc5684ea045ce8245886b01c
Rafael Pardal <rafaelpardal2000@gmail.com>
2022-04-06 17:00:46 +0100

    diretoria dataDefinition adicionada

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text style={styles.hello}>Hello</Text>
+      <Text style={styles.hello}>Hello World</Text>
     </View>
   );
 };
