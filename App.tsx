8e2eacbd02688e7b78102598054095120ec93d5d
Rafael Pardal <rafaelpardal2000@gmail.com>
2022-04-06 21:21:42 +0100

    README.md adicionado com as dependencias

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.hello}>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text style={styles.hello}>Hello World</Text>
+      </View>
+    </NavigationContainer>
+    
   );
 };
 
