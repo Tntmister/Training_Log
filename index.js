b896dd75778aff59c0cbf688bcdd622b3cb90c08
Tntmister <diogoamorim_2011@hotmail.com>
2022-04-08 23:41:14 +0100

    native stack -> stack

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import "react-native-gesture-handler"
 import { AppRegistry } from "react-native"
 import Main from "./src/Main"
 import { name as appName } from "./app.json"
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,19 +1,18 @@
 import React from "react"
 import { Button, StyleSheet, Text, View } from "react-native"
-import { NavigationContainer } from "@react-navigation/native"
 import {
-  NativeStackScreenProps,
-  createNativeStackNavigator
-} from "@react-navigation/native-stack"
+  createStackNavigator,
+  StackScreenProps
+} from "@react-navigation/stack"
 
 type RootStackParamList = {
   Test1: undefined;
   Test2: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>()
+const Stack = createStackNavigator<RootStackParamList>()
 
-type Props = NativeStackScreenProps<RootStackParamList, "Test1">;
+type Props = StackScreenProps<RootStackParamList, "Test1">;
 
 function Test1({ navigation }: Props) {
   return (
@@ -37,12 +36,10 @@ function Test2({ navigation }: Props) {
 
 function Main() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Test1">
-        <Stack.Screen name="Test1" component={Test1} />
-        <Stack.Screen name="Test2" component={Test2} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Test1">
+      <Stack.Screen name="Test1" component={Test1} />
+      <Stack.Screen name="Test2" component={Test2} />
+    </Stack.Navigator>
   )
 }
 
