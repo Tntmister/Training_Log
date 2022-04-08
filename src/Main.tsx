fe4fb078db4c4522debad7479dd810d2bb05dd32
Tntmister <diogoamorim_2011@hotmail.com>
2022-04-08 20:21:15 +0100

    teste navigation + formatacao

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-const App = () => {
+const Stack = createNativeStackNavigator()
+
+const Test1 = ({ navigation }) => {
+  return (
+    <View>
+      <Button title="Test1" onPress={() => navigation.navigate("Test1")} />
+      <Button title="Test2" onPress={() => navigation.navigate("Test2")} />
+      <Text>Test screen 1</Text>
+    </View>
+  )
+}
+
+const Test2 = ({ navigation }) => {
+  return (
+    <View>
+      <Button title="Test1" onPress={() => navigation.navigate("Test1")} />
+      <Button title="Test2" onPress={() => navigation.navigate("Test2")} />
+      <Text>Test screen 2</Text>
+    </View>
+  )
+}
+
+const Main = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text style={styles.hello}>Hello World</Text>
-      </View>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Test1" component={Test1} />
+        <Stack.Screen name="Test2" component={Test2} />
+      </Stack.Navigator>
     </NavigationContainer>
-    
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
-  hello: {
-    textAlign: 'center'
-  }
-});
-
-export default App;
+export default Main
