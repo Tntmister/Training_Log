79c2774e69e21ca35f4bada14eb9d1911e259a26
Tntmister <diogoamorim_2011@hotmail.com>
2022-04-14 16:10:26 +0100

    arranjar import

--- src/app/components/containers/History.tsx
+++ src/app/components/containers/History.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { StatusBar, StyleSheet, Text, View } from "react-native"
-import Style from "../../styles/styles"
+import { theme1 } from "../../styles/styles"
 
 const History = () => {
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor={Style.background[1]}></StatusBar>
+      <StatusBar backgroundColor={theme1.colors.background}></StatusBar>
       <Text style={styles.text}>History</Text>
     </View>
   )
