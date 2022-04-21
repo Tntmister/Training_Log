659a9b6ff39117efa1cb5087484bfddf98efde3e
Tntmister <diogoamorim_2011@hotmail.com>
2022-04-21 23:35:04 +0100

    re-restruturar nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Image } from "react-native"
+import { Dimensions, Image, ImageURISource } from "react-native"
 import React from "react"
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
 import Home from "./screens/Home"
@@ -6,128 +6,71 @@ import Search from "./screens/Search"
 import History from "./screens/History"
 import User from "./screens/User"
 import Train from "./screens/Train"
-import { ThemeContext } from "../App"
+import { useTheme } from "../providers/Theme"
 
 const Tab = createMaterialTopTabNavigator()
 
-const Nav = () => {
-  const theme = React.useContext(ThemeContext)
+export default function Nav() {
+  const theme = useTheme()
 
   return (
     <Tab.Navigator
+      initialRouteName="Train"
+      backBehavior="none"
       tabBarPosition="bottom"
-      screenOptions={{
+      initialLayout={{ width: Dimensions.get("window").width }}
+      screenOptions={({ route }) => ({
         tabBarShowLabel: false,
-        tabBarStyle: { backgroundColor: theme.colors.background }
-      }}
+        tabBarStyle: {
+          backgroundColor: theme.colors.background
+        },
+        tabBarIconStyle: {
+          width: 30,
+          height: 30
+          //borderWidth: 2,
+          //borderColor: "#ffffff"
+        },
+        tabBarIcon: ({ focused }) => (
+          <TabBarIcon
+            focused={focused}
+            image={images[route.name as keyof typeof images]}
+          />
+        )
+      })}
     >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/home/home(-xxxhdpi).png")}
-              resizeMode="contain"
-              style={{
-                width: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                height: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                tintColor: focused ? theme.colors.main : theme.colors.foreground
-              }}
-            />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={Search}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/search/search(-xxxhdpi).png")}
-              resizeMode="contain"
-              style={{
-                width: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                height: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                tintColor: focused ? theme.colors.main : theme.colors.foreground
-              }}
-            />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="Train"
-        component={Train}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/train/train(-xxxhdpi).png")}
-              resizeMode="contain"
-              style={{
-                width: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                height: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                tintColor: focused ? theme.colors.main : theme.colors.foreground
-              }}
-            />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="History"
-        component={History}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/history/history(-xxxhdpi).png")}
-              resizeMode="contain"
-              style={{
-                width: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                height: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                tintColor: focused ? theme.colors.main : theme.colors.foreground
-              }}
-            />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="User"
-        component={User}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={require("../assets/icons/user/user(-xxxhdpi).png")}
-              resizeMode="contain"
-              style={{
-                width: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                height: focused
-                  ? theme.sizes.icon_size_focused
-                  : theme.sizes.icon_size,
-                tintColor: focused ? theme.colors.main : theme.colors.foreground
-              }}
-            />
-          )
-        }}
-      />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Train" component={Train} />
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="User" component={User} />
     </Tab.Navigator>
   )
 }
+const images = {
+  User: require("../assets/icons/user/user(-xxxhdpi).png"),
+  History: require("../assets/icons/history/history(-xxxhdpi).png"),
+  Home: require("../assets/icons/home/home(-xxxhdpi).png"),
+  Search: require("../assets/icons/search/search(-xxxhdpi).png"),
+  Train: require("../assets/icons/train/train(-xxxhdpi).png")
+}
 
-export default Nav
+function TabBarIcon({
+  focused,
+  image
+}: {
+  focused: boolean;
+  image: ImageURISource;
+}) {
+  const theme = useTheme()
+  return (
+    <Image
+      source={image}
+      resizeMode="contain"
+      style={{
+        width: "100%", //focused ? 30 : 25,
+        height: "100%", //focused ? 30 : 25,
+        tintColor: focused ? theme.colors.primary : theme.colors.text
+      }}
+    />
+  )
+}
