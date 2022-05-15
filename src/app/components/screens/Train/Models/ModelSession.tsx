9744ff73115823c671fe0f966599cd58479e769b
Tntmister <diogoamorim_2011@hotmail.com>
2022-05-15 21:39:25 +0100

    remover timer e calcular duracao de sessao no fim

--- src/app/components/screens/Train/Models/ModelSession.tsx
+++ src/app/components/screens/Train/Models/ModelSession.tsx
@@ -1,15 +1,13 @@
 import { StackScreenProps } from "@react-navigation/stack"
-import React, { useContext, useEffect, useRef, useState } from "react"
-import { Alert, ScrollView, View } from "react-native"
-import { Appbar, IconButton, Menu } from "react-native-paper"
-import { RFValue } from "react-native-responsive-fontsize"
+import React, { useContext, useState } from "react"
+import { Alert, ScrollView } from "react-native"
+import { Appbar, Menu } from "react-native-paper"
 import { TrainingSession } from "../../../../../dataDefinition/data"
 import { finishSession } from "../../../../lib/firebaseFS"
 import { useTheme } from "../../../../providers/Theme"
 import { UserContext } from "../../../../providers/User"
 import { Button } from "../../../reusable/Button"
 import MediaCarousel from "../../../reusable/MediaCarousel"
-import { Text } from "../../../reusable/Text"
 import ProgrammedExercise from "../Exercises/ProgrammedExercise"
 import { modelModes } from "../Models/Model"
 import { RootStackParamListModelNav } from "../Models/ModelNav"
@@ -22,15 +20,18 @@ export default function Session({
   const user = useContext(UserContext)!
   const model = route.params.model
 
-  const [session, setSession] = useState<TrainingSession>({
+  const [session] = useState<TrainingSession>({
     ...model,
     exercises: model.exercises.map((ex) => ({ ...ex, userMediaContent: [] })),
     date: Date.now(),
     duration: 0,
-    model: route.params.id && `users/${model.author}/models/${route.params.id}`
+    model: route.params.id
+      ? `users/${model.author}/models/${route.params.id}`
+      : null
   })
 
   function onSessionFinished() {
+    session.duration = session.date - Date.now()
     Alert.alert(
       "Finish Session",
       "Do you want to share this training session publicly?",
@@ -55,23 +56,6 @@ export default function Session({
   }
 
   const [menuVisible, setMenuVisible] = useState(false)
-
-  const [timerActive, setTimerActive] = useState(false)
-  const interval = useRef<NodeJS.Timer>()
-  function onTimerToggle() {
-    if (timerActive) {
-      clearInterval(interval.current!)
-    } else {
-      interval.current = setInterval(() => {
-        setSession({ ...session, duration: session.duration + 1 })
-      }, 1000)
-    }
-    setTimerActive(!timerActive)
-  }
-
-  useEffect(() => {
-    setTimerActive(true)
-  }, [])
 
   return (
     <>
@@ -99,28 +83,6 @@ export default function Session({
           <Menu.Item title={"Placeholder"} />
         </Menu>
       </Appbar>
-      <View
-        style={{
-          width: "100%",
-          height: "10%",
-          alignItems: "center",
-          justifyContent: "space-evenly",
-          flexDirection: "row",
-          alignSelf: "center"
-        }}
-      >
-        <IconButton size={RFValue(30)} icon={"clock"} />
-        <Text style={theme.text.header}>
-          {new Date(session.duration! * 1000)
-            .toISOString()
-            .substring(11, 11 + 8)}
-        </Text>
-        <IconButton
-          size={RFValue(36)}
-          icon={timerActive ? "pause" : "play"}
-          onPress={onTimerToggle}
-        />
-      </View>
       <ScrollView>
         {session.mediaContent.length > 0 && (
           <MediaCarousel assets={session.mediaContent} />
