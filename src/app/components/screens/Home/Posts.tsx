3054e4a10e9a1e81a86a122923410881d25724a5
Tntmister <diogoamorim_2011@hotmail.com>
2022-05-17 14:46:55 +0100

    feed filtrar following

--- src/app/components/screens/Home/Posts.tsx
+++ src/app/components/screens/Home/Posts.tsx
@@ -1,5 +1,5 @@
 import { StackScreenProps } from "@react-navigation/stack"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import {
   TrainingModel,
   TrainingSession
@@ -9,10 +9,12 @@ import { RootStackParamHomeNav } from "./HomeNav"
 import firestore from "@react-native-firebase/firestore"
 import Loading from "../../reusable/Loading"
 import { FlatList } from "react-native"
+import { UserContext } from "../../../providers/User"
 
 export default function Posts({
   navigation
 }: StackScreenProps<RootStackParamHomeNav, "Posts">) {
+  const user = useContext(UserContext)!
   const [posts, setPosts] = useState<(TrainingSession | TrainingModel)[]>([])
 
   const [loading, setLoading] = useState(false)
@@ -21,45 +23,71 @@ export default function Posts({
   const oldest = useRef(Date.now())
   const newest = useRef(Date.now())
 
-  const retrieveData = async () => {
+  // firebase query por conter em lista está limitado a blocos de 10 ids
+  const followingChunks = useRef<string[][]>([])
+  async function initFollowing() {
+    const following = (
+      await firestore()
+        .collection("users")
+        .doc(user.uid)
+        .collection("following")
+        .get()
+    ).docs.map((queryDocumentSnapshot) => queryDocumentSnapshot.id)
+    for (let i = 0; i < following.length; i += 10) {
+      followingChunks.current.push(following.slice(i, i + 10))
+    }
+  }
+
+  async function retrieveData() {
     setLoading(true)
-    const querySnapshot = await firestore()
-      .collection("posts")
-      .orderBy("date", "desc")
-      .startAfter(oldest.current)
-      .limit(10)
-      .get()
-    if (!querySnapshot.empty) {
-      const olderPosts = querySnapshot.docs.map(
-        (document) => document.data() as TrainingModel | TrainingSession
-      )
-      setPosts((newerPosts) => [...newerPosts, ...olderPosts])
-      oldest.current = olderPosts[olderPosts.length - 1].date
+    for await (const querySnapshot of followingChunks.current.map(
+      async (followingChunk) =>
+        await firestore()
+          .collection("posts")
+          .where("author", "in", followingChunk)
+          .orderBy("date", "desc")
+          .startAfter(oldest.current)
+          .limit(10)
+          .get()
+    )) {
+      if (!querySnapshot.empty) {
+        const olderPosts = querySnapshot.docs.map(
+          (document) => document.data() as TrainingModel | TrainingSession
+        )
+        setPosts((newerPosts) => [...newerPosts, ...olderPosts])
+        oldest.current = olderPosts[olderPosts.length - 1].date
+      }
     }
     setLoading(false)
   }
 
-  useEffect(() => {
-    retrieveData()
-  }, [])
-
-  const refreshData = async () => {
+  async function refreshData() {
     setRefreshing(true)
-
-    const querySnapshot = await firestore()
-      .collection("posts")
-      .orderBy("date", "desc")
-      .endBefore(newest.current)
-      .get()
-    if (!querySnapshot.empty) {
-      const newerPosts = querySnapshot.docs.map(
-        (document) => document.data() as TrainingModel | TrainingSession
-      )
-      setPosts((olderPosts) => [...newerPosts, ...olderPosts])
-      newest.current = newerPosts[0].date
+    for await (const querySnapshot of followingChunks.current.map(
+      async (followingChunk) =>
+        await firestore()
+          .collection("posts")
+          .where("author", "in", followingChunk)
+          .orderBy("date", "desc")
+          .endBefore(newest.current)
+          .limit(10)
+          .get()
+    )) {
+      if (!querySnapshot.empty) {
+        const newerPosts = querySnapshot.docs.map(
+          (document) => document.data() as TrainingModel | TrainingSession
+        )
+        setPosts((olderPosts) => [...newerPosts, ...olderPosts])
+        newest.current = newerPosts[0].date
+      }
     }
     setRefreshing(false)
   }
+
+  useEffect(() => {
+    initFollowing()
+    retrieveData()
+  }, [])
 
   const postsFooter = () => {
     return loading ? <Loading /> : null
